82536b81d4201ed89397524b51501ea91dac4f54
Vadym Hulchenko <vadymhulchenko@gmail.com>
2023-11-11 01:22:35 -0700

    implement redux library, add supporting files, save progress

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import './assets/styles/bootstrap.custom.css';
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,6 +21,8 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );

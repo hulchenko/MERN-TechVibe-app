e0e9f308e545a4d188930e3e60290eab77d2edd2
Vadym Hulchenko <vadymhulchenko@gmail.com>
2023-12-01 14:16:28 -0700

    initial cart calculation

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -5,7 +5,32 @@ const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getI
 const cartSlice = createSlice({
     name: "cart",
     initialState,
-    reducers: {}
+    reducers: {
+        addToCart: (state, action) => { //function
+            const item = action.payload;
+            const existItem = state.cartItems.find((x) => x._id === item._id);
+            if (existItem) {
+                state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x);
+            } else {
+                state.cartItems = [...state.cartItems];
+            }
+
+            //Calculate items' price
+            state.itemsPrice = state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0); //0 is the accumulator's default value
+            //Calculate shipping price (if over $100 - free, otherwise $10 for shipping)
+            state.shippingPrice = state.itemsPrice > 100 ? 0 : 10;
+            //Calculate tax price (AB 5%)
+            state.taxPrice = Number((0.05 * state.itemsPrice).toFixed(2));
+            //Calculate total price
+            state.totalPrice = (
+                Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice)
+            ).toFixed(2);
+
+            localStorage.setItem('cart', JSON.stringify(state)); //save state to local storage
+        }
+    }
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;

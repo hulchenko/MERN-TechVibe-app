8d0001dd930355c099a37e50a59c48e2b017ed2d
Vadym Hulchenko <vadymhulchenko@gmail.com>
2023-12-01 15:01:33 -0700

    fix issue where addCart middleware wouldnt update empty state of cartItems, introduce dispatch to utilize middleware

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,25 @@
-import { Link, useParams } from 'react-router-dom';
-import { Row, Image, ListGroup, Card, Button, Col } from 'react-bootstrap';
+import { useState } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { Row, Image, ListGroup, Card, Button, Col, Form } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import { addToCart } from '../slices/cartSlice';
+import { useDispatch } from 'react-redux';
 
 const ProductScreen = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [qty, setQty] = useState(1);
     const { id: productId } = useParams();
     const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
+
+    const addToCartHandler = () => {
+        dispatch(addToCart({ ...product, qty })); //dispatch() is to use addCart middleware and intercept/handle async operations
+        navigate('/cart');
+    };
+
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>
@@ -53,8 +65,25 @@ const ProductScreen = () => {
                                         <Col><strong>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</strong></Col>
                                     </Row>
                                 </ListGroup.Item>
+                                {product.countInStock > 0 && (
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Qty</Col>
+                                            <Col>
+                                                <Form.Control as='select' value={qty} onChange={(e) => setQty(Number(e.target.value))}>
+                                                    {[...Array(product.countInStock).keys()].map((x) => (
+                                                        <option key={x + 1} value={x + 1}>
+                                                            {x + 1}
+                                                        </option>
+                                                    ))}
+                                                </Form.Control>
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                )}
                                 <ListGroup.Item>
                                     <Button className='btn-block' type='button' disabled={product.countInStock === 0}
+                                        onClick={addToCartHandler}
                                     > Add To Cart
                                     </Button>
                                 </ListGroup.Item>
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -7,12 +7,15 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart: (state, action) => { //function
+            console.log(`CALLED 1`, state);
+            console.log(`CALLED 2`, action);
             const item = action.payload;
             const existItem = state.cartItems.find((x) => x._id === item._id);
+            console.log(`TEST: `, existItem);
             if (existItem) {
                 state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x);
             } else {
-                state.cartItems = [...state.cartItems];
+                state.cartItems = [...state.cartItems, item];
             }
 
             //Calculate items' price
@@ -25,6 +28,8 @@ const cartSlice = createSlice({
             state.totalPrice = (
                 Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice)
             ).toFixed(2);
+
+            console.log(`STATE NOW?`, state);
 
             localStorage.setItem('cart', JSON.stringify(state)); //save state to local storage
         }

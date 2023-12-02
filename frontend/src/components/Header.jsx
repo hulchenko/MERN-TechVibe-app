5e42f217cd0988038731abcc7bf45a25209b8069
Vadym Hulchenko <vadymhulchenko@gmail.com>
2023-12-02 12:33:06 -0700

    add cart badge with a total counter

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png';
 
 
 const Header = () => {
+    const { cartItems } = useSelector((state) => state.cart); //this will access store reducer.cart in the store.js file
+
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
@@ -22,6 +25,11 @@ const Header = () => {
                             <LinkContainer to='/cart'>
                                 <Nav.Link>
                                     <FaShoppingCart /> Cart
+                                    {cartItems.length > 0 && (
+                                        <Badge pill bg='warning' style={{ marginLeft: '5px' }}>
+                                            {cartItems.reduce((acc, curr) => acc + curr.qty, 0)}
+                                        </Badge>
+                                    )}
                                 </Nav.Link>
                             </LinkContainer>
                             <LinkContainer to='/login'>

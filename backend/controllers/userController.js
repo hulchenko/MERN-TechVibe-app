322494499657f9e04d36c5d2fb7099b9953de3a1
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-01-07 00:16:47 -0700

    add clear cookies on logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,7 +48,11 @@ const authUser = asyncHandler(async (req, res) => {
 // Private
 
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send('logout user');
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+    res.status(200).json({ message: 'Logged out successfully' });
 });
 
 // Get user profile

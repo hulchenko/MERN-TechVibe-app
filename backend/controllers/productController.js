4bfb0550f76ea18859118b98796acd2d98faab53
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-01-07 01:15:18 -0700

    add status to successful responses, update getUser & updateUser methods

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,14 +4,14 @@ import Product from '../models/productModel.js';
 // Fetch all products -> GET /api/products
 const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({});
-    res.json(products);
+    res.status(200).json(products);
 });
 
 // Fetch a product by id -> GET /api/products/:id
 const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
     if (product) {
-        return res.json(product);
+        return res.status(200).json(product);
     } else {
         res.status(404);
         throw new Error('Resource not found');
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,13 +37,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // Public
 
 const authUser = asyncHandler(async (req, res) => {
-    console.log(`Incoming obj: `, req?.body);
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
     if (user && (await user.matchPassword(password))) {
         generateToken(res, user._id);
-        res.json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -73,7 +72,18 @@ const logoutUser = asyncHandler(async (req, res) => {
 // Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile');
+    const user = await User.findById(req.user._id);
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 
@@ -82,7 +92,25 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // Private
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.send('update user profile');
+    const user = await User.findById(req.user._id);
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+        const updatedUser = await user.save();
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 

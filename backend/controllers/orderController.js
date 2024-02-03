337e7f72fb7c653cd901e68c7228c80d811c39a4
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-02-03 16:18:22 -0700

    init paypal setup for the backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -57,7 +57,26 @@ const getOrderById = asyncHandler(async (req, res) => {
 // PUT api/orders/:id/pay
 
 const updateOrderPaid = asyncHandler(async (req, res) => {
-    res.send('Update paid status of the order');
+    const order = await Order.findById(req.params.id);
+    console.log(`TRIGGER!`);
+
+    if (order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        };
+
+        const updatedOrder = await order.save();
+
+        res.status(200).json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order is not found');
+    }
 });
 
 // Update delivered status of the order

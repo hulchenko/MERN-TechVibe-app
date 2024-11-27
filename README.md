30109f14f187d4ac1579057697697141c133eb33
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-11-27 00:39:53 -0400

    fix the issue where paypal payment threw an error for non-admin user

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ MongoDB
 PayPal API
 
 Note: some browser extensions affect the carousel component
+
+PayPal testing credentials:
+u: sb-vrbav29081913@personal.example.com
+p: Wj=3b%Rx
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,13 +1,13 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-import { addOrderItems, getAllOrders, getOrderById, getMyOrders, updateOrderDelivered, updateOrderPaid } from '../controllers/orderController.js';
-import { protect, admin } from '../middleware/authMiddleware.js';
+import { addOrderItems, getAllOrders, getOrderById, getMyOrders, updateOrderDelivered, updateOrderPaid } from "../controllers/orderController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route('/').get(protect, admin, getAllOrders).post(protect, addOrderItems);
-router.route('/myorders').get(protect, getMyOrders);
-router.route('/:id').get(protect, getOrderById);
-router.route('/:id/pay').put(protect, admin, updateOrderPaid);
-router.route('/:id/deliver').put(protect, admin, updateOrderDelivered);
+router.route("/").get(protect, admin, getAllOrders).post(protect, addOrderItems);
+router.route("/myorders").get(protect, getMyOrders);
+router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderPaid);
+router.route("/:id/deliver").put(protect, admin, updateOrderDelivered);
 
 export default router;

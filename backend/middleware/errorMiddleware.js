2f0c64045ed45d541aaaf944d16d46ed34ccb39c
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-11-28 19:53:06 -0400

    add additional comments for error middleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,23 +1,25 @@
 const notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
+  // Backup error, used only if errorHandler won't be able to handle the error
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
 };
 
 const errorHandler = (error, req, res, next) => {
-    let statusCode = res.statusCode === 200 ? 500 : res.statusCode; //Mongoose returns 200 for "Cast to ObjectId failed" error
-    let message = error.message;
+  // Overwrites default Express error handler: https://expressjs.com/en/guide/error-handling.html
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode; // Mongoose returns 200 for "Cast to ObjectId failed" error
+  let message = error.message;
 
-    // Modify if the message is Mongoose non-existing ObjectId error:
-    if (error.name === 'CastError' && error.kind === 'ObjectId') {
-        message = 'Resource not found';
-        statusCode = 404; //500 by default
-    }
+  // Modify if the message is Mongoose non-existing ObjectId error:
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    message = "Resource not found";
+    statusCode = 404; // 500 by default
+  }
 
-    res.status(statusCode).json({
-        message,
-        stack: process.env.NODE_ENV === 'production' ? 'Hidden' : error.stack,
-    });
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? "Hidden" : error.stack,
+  });
 };
 
 export { notFound, errorHandler };

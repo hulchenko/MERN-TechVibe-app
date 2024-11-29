67a73d4a2c545c920968dae0722bd46b9b63b1d7
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-11-29 14:45:13 -0400

    update error and loading conditions in components renderring

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,39 +1,39 @@
-import { Link } from 'react-router-dom';
-import { Carousel, Image } from 'react-bootstrap';
-import Loader from './Loader';
-import Message from './Message';
-import { useGetTopProductsQuery } from '../slices/productsApiSlice';
-
+import { Link } from "react-router-dom";
+import { Carousel, Image } from "react-bootstrap";
+import Loader from "./Loader";
+import Message from "./Message";
+import { useGetTopProductsQuery } from "../slices/productsApiSlice";
 
 const ProductCarousel = () => {
-    const carouselCaption = {
-        position: 'absolute',
-        width: '100%',
-        left: '0',
-        right: '0',
-        bottom: '0',
-        background: 'rgba(0, 0, 0, 0.5)'
-    };
-    const { data: products, isLoading, error } = useGetTopProductsQuery();
+  const carouselCaption = {
+    position: "absolute",
+    width: "100%",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    background: "rgba(0, 0, 0, 0.5)",
+  };
+  const { data: products, isLoading, error } = useGetTopProductsQuery();
 
-    return isLoading ? <Loader /> : error ? (
-        <Message variant='danger'>{error?.data?.message || error.error}</Message>
-    ) : (
-        <Carousel pause='hover' className='bg-primary mb-4'>
-            {products.map((product) => (
-                <Carousel.Item key={product._id}>
-                    <Link to={`/product/${product._id}`}>
-                        <Image src={product.image} alt={product.name} fluid />
-                        <Carousel.Caption style={carouselCaption}>
-                            <h2 className='text-white text-right'>
-                                {product.name} (${product.price})
-                            </h2>
-                        </Carousel.Caption>
-                    </Link>
-                </Carousel.Item>
-            ))}
-        </Carousel>
-    );
+  if (isLoading) return <Loader />;
+  if (error) return <Message variant="danger">{error?.data?.message || error.error}</Message>;
+
+  return (
+    <Carousel pause="hover" className="bg-primary mb-4">
+      {products.map((product) => (
+        <Carousel.Item key={product._id}>
+          <Link to={`/product/${product._id}`}>
+            <Image src={product.image} alt={product.name} fluid />
+            <Carousel.Caption style={carouselCaption}>
+              <h2 className="text-white text-right">
+                {product.name} (${product.price})
+              </h2>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
 };
 
-export default ProductCarousel;;
+export default ProductCarousel;
--- frontend/src/screens/admin/ProductEditScreen.tsx
+++ frontend/src/screens/admin/ProductEditScreen.tsx
@@ -6,9 +6,8 @@ import FormContainer from "../../components/FormContainer";
 import Loader from "../../components/Loader";
 import Message from "../../components/Message";
 import { useGetProductDetailsQuery, useUpdateProductMutation, useUploadProductImageMutation } from "../../slices/productsApiSlice";
-import { ProductInterface } from "../../interfaces/product.interface";
-import { apiErrorHandler } from "../../utils/errorUtils";
 import { APIError } from "../../types/api-error.type";
+import { apiErrorHandler } from "../../utils/errorUtils";
 
 const ProductEditScreen = () => {
   const { id: productId } = useParams();
@@ -21,9 +20,9 @@ const ProductEditScreen = () => {
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState("");
 
-  const { data: product, isLoading, refetch, error } = useGetProductDetailsQuery(productId);
+  const { data: product, isLoading, refetch, error: getProductError } = useGetProductDetailsQuery(productId);
   const [updateProduct, { isLoading: loadingUpdate }] = useUpdateProductMutation();
-  const [uploadProductImage, { isLoading: loadingUpload }] = useUploadProductImageMutation();
+  const [uploadProductImage] = useUploadProductImageMutation();
 
   const navigate = useNavigate();
 
@@ -75,6 +74,9 @@ const ProductEditScreen = () => {
     }
   };
 
+  if (isLoading) return <Loader />;
+  if (getProductError) return <Message variant="danger">{getProductError}</Message>;
+
   return (
     <>
       <Link to={"/admin/productlist"} className="btn btn-light my-3">
@@ -83,51 +85,45 @@ const ProductEditScreen = () => {
       <FormContainer>
         <h1>Edit Product</h1>
         {loadingUpdate && <Loader />}
-        {isLoading ? (
-          <Loader />
-        ) : error ? (
-          <Message variant="danger">{error}</Message>
-        ) : (
-          <Form onSubmit={submitHandler}>
-            <Form.Group controlId="name" className="my-2">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" value={name} onChange={(e) => setName(e.target.value)}></Form.Control>
-            </Form.Group>
-            <Form.Group controlId="price" className="my-2">
-              <Form.Label>Price</Form.Label>
-              <Form.Control type="number" placeholder="Enter price" value={price} onChange={(e) => setPrice(e.target.value)}></Form.Control>
-            </Form.Group>
-            <Form.Group controlId="image" className="my-2">
-              <Form.Label>Image</Form.Label>
-              <Form.Control type="text" placeholder="Enter image URL" value={image} onChange={(e) => setImage}></Form.Control>
-              <Form.Control type="file" onChange={uploadFileHandler}></Form.Control>
-            </Form.Group>
-            <Form.Group controlId="brand" className="my-2">
-              <Form.Label>Brand</Form.Label>
-              <Form.Control type="text" placeholder="Enter brand" value={brand} onChange={(e) => setBrand(e.target.value)}></Form.Control>
-            </Form.Group>
-            <Form.Group controlId="category" className="my-2">
-              <Form.Label>Category</Form.Label>
-              <Form.Control type="text" placeholder="Enter category" value={category} onChange={(e) => setCategory(e.target.value)}></Form.Control>
-            </Form.Group>
-            <Form.Group controlId="countInStock" className="my-2">
-              <Form.Label>Count In Stock</Form.Label>
-              <Form.Control
-                type="number"
-                placeholder="Enter count in stock"
-                value={countInStock}
-                onChange={(e) => setCountInStock(Number(e.target.value))}
-              ></Form.Control>
-            </Form.Group>
-            <Form.Group controlId="description" className="my-2">
-              <Form.Label>Description</Form.Label>
-              <Form.Control type="text" placeholder="Enter description" value={description} onChange={(e) => setDescription(e.target.value)}></Form.Control>
-            </Form.Group>
-            <Button type="submit" variant="primary" className="my-2">
-              Update
-            </Button>
-          </Form>
-        )}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name" className="my-2">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter name" value={name} onChange={(e) => setName(e.target.value)}></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="price" className="my-2">
+            <Form.Label>Price</Form.Label>
+            <Form.Control type="number" placeholder="Enter price" value={price} onChange={(e) => setPrice(e.target.value)}></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="image" className="my-2">
+            <Form.Label>Image</Form.Label>
+            <Form.Control type="text" placeholder="Enter image URL" value={image} onChange={(e) => setImage}></Form.Control>
+            <Form.Control type="file" onChange={uploadFileHandler}></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="brand" className="my-2">
+            <Form.Label>Brand</Form.Label>
+            <Form.Control type="text" placeholder="Enter brand" value={brand} onChange={(e) => setBrand(e.target.value)}></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="category" className="my-2">
+            <Form.Label>Category</Form.Label>
+            <Form.Control type="text" placeholder="Enter category" value={category} onChange={(e) => setCategory(e.target.value)}></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="countInStock" className="my-2">
+            <Form.Label>Count In Stock</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter count in stock"
+              value={countInStock}
+              onChange={(e) => setCountInStock(Number(e.target.value))}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="description" className="my-2">
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="text" placeholder="Enter description" value={description} onChange={(e) => setDescription(e.target.value)}></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="primary" className="my-2">
+            Update
+          </Button>
+        </Form>
       </FormContainer>
     </>
   );

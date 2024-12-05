e60f9d81b0224701630dd2b994e94d44c197ec43
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-12-05 16:53:45 -0400

    rework product screen

--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -1,5 +1,19 @@
-import { useState } from "react";
-import { Button, Card, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Image, Input, Divider, Textarea } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Card,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Image,
+  Input,
+  Divider,
+  Textarea,
+  CardHeader,
+  CardFooter,
+  CardBody,
+} from "@nextui-org/react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import Loader from "../components/Loader";
@@ -12,6 +26,8 @@ import { useCreateReviewMutation, useGetProductDetailsQuery } from "../slices/pr
 import { APIError } from "../types/api-error.type";
 import { apiErrorHandler } from "../utils/errorUtils";
 
+const ratingOptions = ["Poor", "Fair", "Good", "Very Good", "Excellent"];
+
 const ProductScreen = () => {
   const { id: productId } = useParams();
 
@@ -19,7 +35,8 @@ const ProductScreen = () => {
   const navigate = useNavigate();
 
   const [qty, setQty] = useState(1);
-  const [rating, setRating] = useState<number | null>(null);
+  const [rating, setRating] = useState<null | number>(null);
+  const [selectedRating, setSelectedRating] = useState("Rating");
   const [comment, setComment] = useState("");
 
   const { data: product, isLoading, refetch, error } = useGetProductDetailsQuery(productId || "");
@@ -34,14 +51,19 @@ const ProductScreen = () => {
 
   const submitHandler = async (e: React.FormEvent) => {
     e.preventDefault();
+    if (!rating || !comment) {
+      toast.error("Both field are required");
+      return;
+    }
     try {
-      await createReview({
+      const response = await createReview({
         productId,
         rating: rating || 0,
         comment,
-      }).unwrap();
+      }).unwrap(); // unwraps {data: {message: ""}} -> {message: ""}
+
       refetch();
-      toast.success("Review submitted!");
+      toast.success(response?.message || "Review submitted");
       setRating(0);
       setComment("");
     } catch (error) {
@@ -59,85 +81,112 @@ const ProductScreen = () => {
         Back
       </Button>
 
-      <Card>
-        <Image src={product.image} alt={product.name} />
-        <Rating value={product.rating || 0} text={`${product.numReviews} reviews`} />
-        <Input isReadOnly label="Price" variant="bordered" defaultValue={`$${product.price}`} className="max-w-xs" />
-        <Input isReadOnly label="Description" variant="bordered" defaultValue={product.description} className="max-w-xs" />
-      </Card>
-      <Card>
-        <Input isReadOnly label="Price" variant="bordered" defaultValue={`$${product.price}`} className="max-w-xs" />
-        <Input isReadOnly label="Status" variant="bordered" defaultValue={product.countInStock > 0 ? "In Stock" : "Out Of Stock"} className="max-w-xs" />
-        <Input isReadOnly label="Qty" variant="bordered" defaultValue={product.countInStock > 0 ? "In Stock" : "Out Of Stock"} className="max-w-xs" />
+      <div className="flex gap-4 justify-between h-96 mt-6">
+        <div className="flex-col h-full w-full">
+          <Image src={product.image} alt={product.name} height="40rem" />
+          <Rating value={product.rating || 0} text={`${product.numReviews} review(s)`} />
+          <Divider className="mt-6" />
+          <h2 className="my-6 font-bold">Write a customer review</h2>
+          {loadingReview && <Loader />}
+          {userInfo ? (
+            <form onSubmit={submitHandler} className="flex flex-col gap-4">
+              <Dropdown>
+                <DropdownTrigger>
+                  <Button color="primary" variant="faded" className="capitalize">
+                    {selectedRating}
+                  </Button>
+                </DropdownTrigger>
+                <DropdownMenu onAction={(key) => setSelectedRating(String(key))}>
+                  {ratingOptions.map((key, idx) => (
+                    <DropdownItem value={idx + 1} key={key} onClick={(e) => setRating(e.currentTarget.value)}>
+                      {key}
+                    </DropdownItem>
+                  ))}
+                </DropdownMenu>
+              </Dropdown>
+              <Textarea required label="Comment" placeholder="Add a comment" className="w-full" onChange={(e) => setComment(e.target.value)} />
+              <Button isDisabled={loadingReview} type="submit" color="primary" className="max-w-6">
+                Submit
+              </Button>
+            </form>
+          ) : (
+            <Message>
+              Please{" "}
+              <Link to="/login" className="text-violet-500">
+                sign in
+              </Link>{" "}
+              to write a review
+            </Message>
+          )}
+        </div>
 
-        <Dropdown as="select">
-          <DropdownTrigger>
-            <Button variant="faded">{qty}</Button>
-          </DropdownTrigger>
-          <DropdownMenu onAction={(key) => setQty(Number(key))}>
-            {Array(product.countInStock > 10 ? 10 : product.countInStock)
-              .fill(0)
-              .map((_, idx) => (
-                <DropdownItem key={idx + 1} value={idx + 1}>
-                  {idx + 1}
-                </DropdownItem>
+        <div className="w-full">
+          <h2 className="my-6 font-bold">Description</h2>
+          <Card className="flex gap-2">
+            <CardBody>
+              <p>{product.description}</p>
+            </CardBody>
+          </Card>
+          <Divider className="mt-6" />
+          <h2 className="my-6 font-bold">Customer comments</h2>
+          <div className="h-full overflow-auto">
+            {product?.reviews?.length === 0 && <Message>No Comments</Message>}
+            {product.reviews &&
+              product.reviews.map((review: ReviewInterface) => (
+                <Card key={review._id} className="mt-4">
+                  <CardHeader>
+                    <h2 className="font-bold">{review.name}</h2>
+                  </CardHeader>
+                  <CardBody>
+                    <Rating value={review.rating} text={""} />
+                    <p>{review?.createdAt?.substring(0, 10)}</p>
+                  </CardBody>
+                  <CardFooter>
+                    <p>{review.comment}</p>
+                  </CardFooter>
+                </Card>
               ))}
-          </DropdownMenu>
-        </Dropdown>
+          </div>
+        </div>
 
-        <Button className="btn-block" variant="bordered" color="primary" type="button" isDisabled={product.countInStock === 0} onClick={addToCartHandler}>
-          Add To Cart
-        </Button>
-      </Card>
-      <Card>
-        <h2>Reviews</h2>
-        {product?.reviews?.length === 0 && <Message>No Reviews</Message>}
-
-        {product.reviews &&
-          product.reviews.map((review: ReviewInterface) => (
-            <Card key={review._id}>
-              <strong>{review.name}</strong>
-              <Rating value={review.rating} text={""} />
-              <p>{review?.createdAt?.substring(0, 10)}</p>
-              <p>{review.comment}</p>
-            </Card>
-          ))}
-        <Divider />
-        <h2>Write a Customer Review</h2>
-
-        {loadingReview && <Loader />}
-
-        {userInfo ? (
-          <form onSubmit={submitHandler}>
-            <h2>Rating</h2>
-            <Dropdown>
-              <DropdownTrigger>
-                <Button color="primary" variant="faded">
-                  {rating || "Select"}
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu onAction={(key) => setRating(Number(key))}>
-                <DropdownItem value="1">Poor</DropdownItem>
-                <DropdownItem value="2">Fair</DropdownItem>
-                <DropdownItem value="3">Good</DropdownItem>
-                <DropdownItem value="4">Very Good</DropdownItem>
-                <DropdownItem value="5">Excellent</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-
-            <h2>Comment</h2>
-            <Textarea label="Comment" placeholder="Add a comment" className="max-w-xs" onChange={(e) => setComment(e.target.value)} />
-
-            <Button isDisabled={loadingReview} type="submit" color="primary">
-              Submit
-            </Button>
-          </form>
-        ) : (
-          <Message>
-            Please <Link to="/login">sign in</Link> to write a review
-          </Message>
-        )}
-      </Card>
+        <div className="w-full flex justify-center">
+          <Card className="h-60 w-1/2">
+            <CardHeader className="flex justify-between">
+              <h2>Price</h2>
+              <p className="font-bold">${product.price}</p>
+            </CardHeader>
+            <CardBody className="flex-col">
+              <div className="flex justify-between">
+                <h2>Status</h2>
+                <p className="font-bold">{product.countInStock > 0 ? "In Stock" : "Out Of Stock"}</p>
+              </div>
+              <Divider className="my-4" />
+              <div className="flex justify-between">
+                <h2>Qty</h2>
+                <Dropdown as="select">
+                  <DropdownTrigger>
+                    <Button variant="faded">{qty}</Button>
+                  </DropdownTrigger>
+                  <DropdownMenu onAction={(key) => setQty(Number(key))}>
+                    {Array(product.countInStock > 10 ? 10 : product.countInStock)
+                      .fill(0)
+                      .map((_, idx) => (
+                        <DropdownItem key={idx + 1} value={idx + 1}>
+                          {idx + 1}
+                        </DropdownItem>
+                      ))}
+                  </DropdownMenu>
+                </Dropdown>
+              </div>
+            </CardBody>
+            <CardFooter className="flex-col">
+              <Button className="btn-block" variant="bordered" color="primary" type="button" isDisabled={product.countInStock === 0} onClick={addToCartHandler}>
+                Add To Cart
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
     </>
   );
 };

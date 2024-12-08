bd837f89a89ce523c452d5f1877dcb25cb68235a
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-12-08 16:08:12 -0400

    fix footer z-index issue covering paginator

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "@nextui-org/react";
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="absolute bottom-0 w-full text-center">
+    <footer className="absolute bottom-0 w-full text-center z-10">
       &copy; {year} by{" "}
       <Link isBlock showAnchorIcon target="_blank" href="https://github.com/hulchenko" className="font-bold text-violet-500">
         hulchenko

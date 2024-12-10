232a6989e810329e6baf6e9c11d2baebc358497d
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-12-09 20:52:13 -0400

    introduce home page greeting

---
+++ frontend/src/components/Greeting.tsx
@@ -0,0 +1,12 @@
+const Greeting = () => {
+  return (
+    <div className="inline-block max-w-xl text-center justify-center">
+      <span className="text-5xl font-bold tracking-tight">Discover the joy of </span>
+      <span className="text-5xl font-semibold tracking-tight bg-clip-text text-transparent bg-gradient-to-b from-[#FF1CF7] to-[#b249f8]">reading.</span>
+      <br />
+      <div className="w-full my-2 text-lg lg:text-xl text-default-600 block mt-4">Explore, shop, and enjoy a mix of modern books and timeless classics.</div>
+    </div>
+  );
+};
+
+export default Greeting;

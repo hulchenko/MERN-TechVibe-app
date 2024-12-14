040fc61c8b394edddb02ce17e47f8670587bc50d
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-12-14 13:42:36 -0400

    add build script & production configuration

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,51 @@
-import express from 'express';
-import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import path from 'path';
+import express from "express";
+import path from "path";
+import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
+import orderRoutes from "./routes/orderRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+// Init setup
 dotenv.config();
-
 connectDB(); //connect to mongoDB
-const app = express();
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve(); // set current directory as __dirname
+const app = express();
 
-// Body parser middleware
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Cookie parser middleware
 app.use(cookieParser()); // allows to access req.cookies.cookie_name
 
-app.get('/', (req, res) => {
-    res.send('API is running!');
-});
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.get("/api/config/paypal", (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/upload', uploadRoutes);
+if (process.env.NODE_ENV === "production") {
+  // provide express with React build folder
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }));
+  // if requesting route is not /api or /uploads - redirect to index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  // dev server
+  app.get("/", (req, res) => {
+    res.send("API is running!");
+  });
+}
 
-const __dirname = path.resolve(); // set current directory as __dirname
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
+// Additional middleware for missing routes/error handling
 app.use(notFound);
 app.use(errorHandler);
 
